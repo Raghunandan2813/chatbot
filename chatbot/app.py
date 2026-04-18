0c1bf77a0e8818b5f9cfe0c547c398cccbbe441d
Raghunandan <raghu9555k@gmail.com>
2026-04-18 21:19:52 +0530

    inital commit

---
+++ chatbot/app.py
@@ -0,0 +1,33 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import  ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the user queries"),
+        ("user", "Question:{question}")
+    ]
+)
+
+
+st.title('Langchain Demo with GROQ API')
+input_text=st.text_input("Search the topic u want")
+
+
+llm=ChatGroq(model="llama-3.1-8b-instant")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
